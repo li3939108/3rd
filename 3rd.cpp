dbb8ee2a9b901f0993af0128cd7b75839baffb84
li3939108 <li3939108@gmail.com>
2016-01-29 00:31:18 -0600

    update

--- 3rd.cpp
+++ 3rd.cpp
@@ -1,12 +1,17 @@
 #include "parser_helper.h"
 #include <iostream>
 #include <string>
+#include <string.h>
 #include <assert.h>
+#include <map>
+#include <vector>
 
 using namespace std;
 
 enum gate_type{
-    INV = 323,
+	ILLEGAL_GATE_TYPE = 323,
+	DFF,
+    INV ,
     NAND2,
     NAND3,
     NAND4,
@@ -31,50 +36,114 @@ enum gate_type{
     FFMS,
     AND2,
     AND3,
+	AND4,
+	AND5,
+	AND6,
+	AND7,
     OR2,
     OR3,
+	OR4,
+	OR5,
+	OR6,
 } ;
+
+enum gate_type str2type(string t ){
+	char *tok ;
+	string tokstr ;
+	const char *ct = t.c_str() ;
+	char ctcopy[128] ;
+	strcpy(ctcopy, ct);
+	tok = strtok(ctcopy, "_") ;
+	assert(tok != NULL) ;
+	tokstr = string(tok) ;
+	cout << "token " << tokstr << endl ;
+	if(tokstr.substr(0, 2).compare( "OR" ) == 0  ){
+		return (enum gate_type)(OR2 + tok[2] - '2') ;
+	}else if(tokstr.substr(0, 3).compare("NOR") == 0  ){
+		return (enum gate_type)(NOR2 + tok[3] - '2') ;
+	}else if(tokstr.substr(0, 3).compare( "AND" ) == 0  ){
+		return (enum gate_type)(AND2 + tok[3]  - '2' ) ;
+	}else if(tokstr.substr(0, 3).compare( "INV" ) == 0 ){
+		return INV;
+	}else if(tokstr.substr(0, 3).compare( "DFF" ) == 0 ) {
+		return DFF ;
+	}else if(tokstr.substr(0, 4).compare( "NAND" ) == 0){
+		return (enum gate_type)(NAND2 + tok[4] - '2' );
+	}
+	return ILLEGAL_GATE_TYPE; 
+}
 
 enum net_type {
 	PI = 111 ,
 	PO ,
 	WIRE,
 };
+enum fanout_in {
+	ILLEGAL_FAN = 1,
+	FANOUT,
+	FANIN ,
+};
+class Net {
+	enum net_type type ;
+	string driver ;
+	vector<string > load ;
+public:
+	string id ;
+	Net(string id, enum net_type type);
+	Net() {} 
+	void print();
+};
+
 
 class Gate {
-	string id ;
 	enum gate_type type ;
 	vector<class Gate > fanouts ;
 	vector<class Gate > fanins ;
 	vector<class Net > fanout_nets ;
 	vector<class Net > fanin_nets ;
-	Gate(string id, enum gate_type type);
-};
-class Net {
-	string id ;
-	enum net_type type ;
-	string driver ;
-	vector<string > load ;
 public:
-	Net(string id, enum net_type type);
-	void print();
+	string id ;
+	Gate() {} 
+	Gate(string id, enum gate_type type);
+	void add_nets(map<string, class Net> nets_hash, string id, enum fanout_in ) ;
+	void print() ;
 };
-
+void Gate::print(){
+	cout << "Gate: " <<  this->id << " | Type: "<< (int)(this->type) << endl ;
+	cout << "\tfanin : " ;
+	for(auto it = this->fanin_nets.begin() ; it != this->fanin_nets.end() ; it++){
+		cout << it->id << ", " ;
+	}
+	cout << "\n\tfanout : " ;
+	for(auto it = this->fanout_nets.begin() ; it != this->fanout_nets.end() ; it++){
+		cout << it->id << ", " ;
+	}
+	cout << endl ;
+}
+void Gate::add_nets(map<string, class Net> nets_hash, string id, enum fanout_in foi ) {
+	if(foi == FANIN){
+		this->fanin_nets.push_back( nets_hash[ id ] ) ;
+	}else if(foi == FANOUT){
+		this->fanout_nets.push_back( nets_hash[ id ] ) ;
+	}
+}
 Gate::Gate(string id, enum gate_type type){
 	this->id = string(id);
 	this->type = type ;
 }
+
 Net::Net(string id, enum net_type type){
 	this->id = string(id) ;
 	this->type = type ;
 }
 void Net::print(){
-	cout << "net: " << this->id << endl;
-	cout << "type: " << (int)(this->type)  << endl;
+	cout << "net: " << this->id << " | type: " << (int)(this->type)  << endl;
 }
 int main(int argc, char **argv){
 	vector<class Gate> gates ;
 	vector<class Net> nets ;
+	std::map<string, class Net> nets_hash ;
+	std::map<string, class Gate> gates_hash ;
 
 	string net (128, 0);
 
@@ -96,8 +165,10 @@ int main(int argc, char **argv){
 		valid = verilog_parser.read_primary_input (primaryInput) ;
 
 		if (valid){
+			Net n(primaryInput, PI);
 			cout << "Primary input: " << primaryInput << endl ;
-			nets.push_back( Net(primaryInput, PI) ) ;
+			nets.push_back( n ) ;
+			nets_hash[primaryInput] = n ;
 		}
 	} while (valid) ;
 	cout << endl ;
@@ -107,8 +178,10 @@ int main(int argc, char **argv){
 		valid = verilog_parser.read_primary_output (primaryOutput) ;
 
 		if (valid){
+			Net n(primaryOutput, PO) ;
 			cout << "Primary output: " << primaryOutput << endl ;
-			nets.push_back( Net(primaryOutput, PO) ) ;
+			nets.push_back( n ) ;
+			nets_hash[primaryOutput] = n ;
 		}
 
 	} while (valid) ;
@@ -119,12 +192,13 @@ int main(int argc, char **argv){
 		valid = verilog_parser.read_wire (net) ;
 
 		if (valid){
+			Net n(net, WIRE) ;
 			cout << "Net: " << net << endl ;
-			nets.push_back( Net(net, WIRE) ) ;
+			nets.push_back( n ) ;
+			nets_hash [ net ] = n ;
 		}
 
 	} while (valid) ;
-
 
 	cout << endl ;
 	cout << "Cell insts: " << std::endl ;
@@ -136,16 +210,28 @@ int main(int argc, char **argv){
 		valid = verilog_parser.read_cell_inst (cellType, cellInst, pinNetPairs) ;
 
 		if (valid) {
+			Gate g (cellInst, str2type(cellType) ) ;
 			cout << cellType << " " << cellInst << " " ;
 			for (int i=0; i < pinNetPairs.size(); ++i) {
+				if(pinNetPairs[i].first.compare("ZN" ) == 0|| 
+				pinNetPairs[i].first.compare( "Q" ) == 0 ) {
+					g.add_nets(nets_hash, pinNetPairs[i].second, FANOUT) ;
+				}else{
+					g.add_nets(nets_hash, pinNetPairs[i].second, FANIN) ;
+				}
 				cout << "(" << pinNetPairs[i].first << " " << pinNetPairs[i].second << ") " ;
 			}
+			gates.push_back(g ) ;
+			gates_hash[ cellInst ] = g ;
 
 			cout << endl ;
 		}
 		
 	} while (valid) ;
 	for(auto it = nets.begin(); it != nets.end() ; it ++){
+		it->print() ;
+	}
+	for(auto it = gates.begin(); it != gates.end() ; it ++){
 		it->print() ;
 	}
 }
--- Makefile
+++ Makefile
@@ -2,4 +2,4 @@ target=3rd
 cc=clang++
 
 $(target) :  3rd.cpp parser_helper.cpp
-	$(cc) -std=c++11 -o $@ $+
+	$(cc) -g -std=c++11 -o  $@ $+
